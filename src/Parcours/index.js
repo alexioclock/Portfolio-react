b200180f451b952f941cd1bc9d7aac0af529d117
Shootingvision <shootingvision@gmail.com>
2021-12-02 17:58:11 +0100

    add transition aos

--- src/Parcours/index.js
+++ src/Parcours/index.js
@@ -1,17 +1,25 @@
 import "./parcours.scss";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function Parcours() {
+
+    useEffect(() => {
+        Aos.init({duration:2000});
+    }, []);
+
   return (
     <div className="parcours-container">
         <div className="parcours-content">
-            <h1>Expériences professionnelles</h1>
-                <h2>Stage Développeur Front-End chez Cénotélie // 6 mois</h2>
-                    <p>Re-conception, développement, intégration, test maintenance corrective et évolutive, mise en production. Technos : React, TypeScript</p>
-                <h2>Formation O'clock développeur web fullstack // 6 mois</h2> 
-                    <p>Formation à temps plein avec la spécialité React/Redux</p>
-                    <p>Consulter tout le programme <a href="https://oclock.io/formations/developpeur-web" target="_blank" >ici</a></p>
-                <h2>Rédaction web SEO // 5 ans</h2>
-                    <p>D'abord sous contrat puis en indépendant, je me suis occupé de la gestion du catalogue, définition de la stratégie de contenu, analyse de l'audience et du positionnement, optimisation du contenu (SEO), analyse de l'audience et du positionnement (Google Analytics, SEM RUSH, Search Console)</p>
+            <h1 data-aos="fade-left">Expériences professionnelles</h1>
+                <h2 data-aos="fade-left">Stage Développeur Front-End chez Cénotélie // 6 mois</h2>
+                    <p data-aos="fade-right">Re-conception, développement, intégration, test maintenance corrective et évolutive, mise en production. Technos : React, TypeScript</p>
+                <h2 data-aos="fade-left">Formation O'clock développeur web fullstack // 6 mois</h2> 
+                    <p data-aos="fade-right">Formation à temps plein avec la spécialité React/Redux</p>
+                    <p data-aos="fade-right">Consulter tout le programme <a href="https://oclock.io/formations/developpeur-web" target="_blank" >ici</a></p>
+                <h2 data-aos="fade-left">Rédaction web SEO // 5 ans</h2>
+                    <p data-aos="fade-right">D'abord sous contrat puis en indépendant, je me suis occupé de la gestion du catalogue, définition de la stratégie de contenu, analyse de l'audience et du positionnement, optimisation du contenu (SEO), analyse de l'audience et du positionnement (Google Analytics, SEM RUSH, Search Console)</p>
         </div>
     </div>
   );
