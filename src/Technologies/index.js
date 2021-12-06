79e093a9bdabb2fd3b4cccb97fc59a6349fadb40
Shootingvision <shootingvision@gmail.com>
2021-12-06 12:04:45 +0100

    border contact and update slidemenu

--- src/Technologies/index.js
+++ src/Technologies/index.js
@@ -8,12 +8,12 @@ function Techologies() {
           Les technologies utilisées
         </p>
         <div className="list-container">
-            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/html-5--v1.png"/></div>
-            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/css3.png"/></div>
-            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/react-native--v1.png"/></div>
-            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/git.png"/></div>
-            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios-glyphs/480/000000/github.png"/></div>
-            <div className="circle"><img  className="skill-logo" src="https://img.icons8.com/windows/512/000000/redux.png"/></div>
+            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/html-5--v1.png" alt=""/></div>
+            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/css3.png" alt=""/></div>
+            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/react-native--v1.png" alt=""/></div>
+            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios/500/000000/git.png" alt=""/></div>
+            <div className="circle"><img className="skill-logo" src="https://img.icons8.com/ios-glyphs/480/000000/github.png" alt=""/></div>
+            <div className="circle"><img  className="skill-logo" src="https://img.icons8.com/windows/512/000000/redux.png" alt=""/></div>
         </div>
       </div>
     </div>
